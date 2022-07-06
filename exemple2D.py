28121d7b4a4f2ce2c63b277985b70a4a1d4e1426
pierreChauris <107399315+pierreChauris@users.noreply.github.com>
2022-07-06 16:30:58 +0200

    Add files via upload

---
+++ exemple2D.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 21 13:41:21 2022
+
+@author: pierre chauris
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from findiff import FinDiff
+
+
+
+def T_direct(x1,x2):
+    s = x1**2+x2**2+1
+    z = np.array([s,x1/s,x2/s])
+    return z
+
+def T_inverse(z1,z2,z3):
+    return np.array([z1*z2,z1*z3])
+
+def dT_dx(x1,x2):
+    den = (x1**2+x2**2+1)**2
+    return np.array([[2*x1,2*x2],
+                     [(x2**2-x1**2+1)/den,-2*x1*x2/den],
+                     [-2*x1*x2/den,(x1**2-x2**2+1)/den]])
+def dT_dz(x1,x2):
+    z = T_direct(x1,x2)
+    z1,z2,z3 = z
+    return np.array([[z2,z1,0],[z3,0,z1]])
+    
+def dT_dz_emp_ij(i,j):
+    return np.linalg.pinv(dTdx[:,:,i,j])
+
+def dT_dz_emp():
+    dTdz_emp = np.array([dT_dz_emp_ij(i,j) for (i,j) in flat_index])
+    return np.reshape(dTdz_emp,(N,N,2,3))
+
+def Tinv_est(theta,z):
+    a1,b1,c1,a2,b2,c2 = theta
+    z1,z2,z3 = z
+    return np.array([a1*z1*z2 + b1*z1*z3 + c1*z2*z3
+                    ,a2*z1*z2 + b2*z1*z3 + c2*z2*z3])
+    
+def dJ_da1(theta):
+    arg = np.array([[Z[0,i,j]*Z[1,i,j]*((Tinv_est(theta,Z[:,i,j]))[0]-x1[j]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+def dJ_db1(theta):
+    arg = np.array([[Z[0,i,j]*Z[2,i,j]*((Tinv_est(theta,Z[:,i,j]))[0]-x1[j]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+def dJ_dc1(theta):
+    arg = np.array([[Z[1,i,j]*Z[2,i,j]*((Tinv_est(theta,Z[:,i,j]))[0]-x1[j]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+def dJ_da2(theta):
+    arg = np.array([[Z[0,i,j]*Z[1,i,j]*((Tinv_est(theta,Z[:,i,j]))[1]-x1[i]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+def dJ_db2(theta):
+    arg = np.array([[Z[0,i,j]*Z[2,i,j]*((Tinv_est(theta,Z[:,i,j]))[1]-x1[i]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+def dJ_dc2(theta):
+    arg = np.array([[Z[1,i,j]*Z[2,i,j]*((Tinv_est(theta,Z[:,i,j]))[1]-x1[i]) for i in range(N)] for j in range(N)])
+    return np.sum(arg)/N**2
+
+# define the grid and compute T(x)=Z and dT/dx on the grid-------------------------------
+N = 100
+index = [[(i,j) for i in range(N)]for j in range(N)]
+flat_index = [num for row in index for num in row]
+x1 = np.linspace(-1,1,N)
+x2 = x1
+X1,X2 = np.meshgrid(x1,x2)
+
+Z = T_direct(X1,X2)
+dTdx = dT_dx(X1,X2)
+dTdz = dT_dz(X1,X2)
+dTdz_emp = dT_dz_emp()
+
+
+# optimize the parameters of Tinv_est
+
+alpha = 0.1
+theta0 = np.array([0,1,0.1,1,0,0.1])
+J = np.array([dJ_da1(theta0),dJ_db1(theta0),dJ_dc1(theta0),dJ_da2(theta0),dJ_db2(theta0),dJ_dc2(theta0)])
+
+a1 = [theta0[0]]
+b1 = [theta0[1]]
+c1 = [theta0[2]]
+a2 = [theta0[3]]
+b2 = [theta0[4]]
+c2 = [theta0[5]]
+Cost = [np.linalg.norm(J)]
+
+niter = 0
+while(np.linalg.norm(J)>0.0001):
+    print(np.linalg.norm(J))
+    niter += 1
+    theta0 = theta0 - alpha*J
+    J = np.array([dJ_da1(theta0),dJ_db1(theta0),dJ_dc1(theta0),dJ_da2(theta0),dJ_db2(theta0),dJ_dc2(theta0)])
+    a1.append(theta0[0])
+    b1.append(theta0[1])
+    c1.append(theta0[2])
+    a2.append(theta0[3])
+    b2.append(theta0[4])
+    c2.append(theta0[5])
+    Cost.append(np.linalg.norm(J))
+    
+i,j = [10,56]
+x_true = np.array([x1[i],x2[j]])
+x_est = Tinv_est(theta0,T_direct(x_true[0],x_true[1]))
+print('theta :',theta0)
+print('x_true :',x_true)
+print('x_est :',x_est)
+
+img,ax = plt.subplots(2,3)
+ax[0,0].plot(a1)
+ax[0,1].plot(b1)
+ax[0,2].plot(c1)
+ax[1,0].plot(a2)
+ax[1,1].plot(b2)
+ax[1,2].plot(c2)
---
+++ exempleR2R3.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 13:51:05 2022
+
+@author: pchauris
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.neural_network import MLPRegressor
+#%% Def classes et fonctions
+class Cell:
+    def __init__(self,index,level):
+        self.level = level
+        self.index = index
+    def info(self):
+        print('index :',self.index)
+        print('level :',self.level)
+    def split(self):
+        C00 = Cell(np.concatenate((self.index,[0,0])),self.level + 1)
+        C01 = Cell(np.concatenate((self.index,[0,1])),self.level + 1)
+        C10 = Cell(np.concatenate((self.index,[1,0])),self.level + 1)
+        C11 = Cell(np.concatenate((self.index,[1,1])),self.level + 1)
+        return C00,C01,C10,C11
+    def center(self):
+        index = self.index
+        level = self.level
+        x1 = 0
+        x2 =  0
+        for k in range(level):
+            x1 += index[2*k]*L/2**k/N
+            x2 += index[2*k+1]*L/2**k/N
+        x1 += L/N/2**(k+1) + x10
+        x2 += L/N/2**(k+1) + x20
+        return np.array([x1,x2])
+
+def cen(grid):
+    X1 = []
+    X2 = []
+    for cell in grid:
+        x1,x2 = cell.center()
+        X1.append(x1)
+        X2.append(x2)
+    return np.array(X1),np.array(X2)
+
+def init_grid(N):
+    grid = []
+    for i in range(N):
+        for j in range(N):
+            grid.append(Cell(np.array([i,j]),1))
+    return grid
+
+
+def crit_sequence(grid):
+    res = []
+    for cell in grid:
+        j = jacobien(cell)
+        res.append(j)
+    return np.array(res)
+
+def alpha_sequence(grid):
+    res = crit_sequence(grid)
+    return np.linspace(0,res.mean(),len(grid))
+
+def distrib_sequence(grid):
+    alpha = alpha_sequence(grid)
+    crit = crit_sequence(grid)
+    res = []
+    for j in range(alpha.size):
+        dj = np.count_nonzero(crit>alpha[j])
+        res.append(dj)
+    return np.array(res)
+
+def auto_threshold(grid):
+    alpha = alpha_sequence(grid)
+    d = distrib_sequence(grid)
+    f = alpha*d
+    #maximum global
+    fmax = np.max(f)
+    idmax = np.where(f == fmax)
+    idmax = idmax[0][0]
+    #alpha 
+    alphamax = alpha[idmax]
+    return alphamax
+
+def iterate_grid(grid,alpha):
+    new_grid = grid.copy()
+    for cell in grid:
+        k = grid.index(cell)
+        J = jacobien(cell)
+        if J > alpha:
+            C00,C01,C10,C11 = cell.split()
+            new_grid.remove(cell)
+            new_grid.insert(k,C11)
+            new_grid.insert(k,C10)
+            new_grid.insert(k,C01)
+            new_grid.insert(k,C00)
+    return new_grid
+
+def T_direct(x1,x2):
+    s = x1**2+x2**2+1
+    z = np.array([s,x1/s,x2/s])
+    return np.transpose(z)
+
+def T_inverse(z1,z2,z3):
+    return np.array([z1*z2,z1*z3])
+
+def dT_dx(x1,x2):
+    den = (x1**2+x2**2+1)**2
+    return np.array([[2*x1,2*x2],
+                     [(x2**2-x1**2+1)/den,-2*x1*x2/den],
+                     [-2*x1*x2/den,(x1**2-x2**2+1)/den]])
+def dT_dz(x1,x2):
+    z1,z2,z3 = T_direct(x1,x2)
+    return np.array([[z2,z1,0],[z3,0,z1]])
+
+def jacobien(cell):
+    x1,x2 = cell.center()
+    J = dT_dx(x1,x2)
+    norme = np.mean(J)
+    return np.sqrt(J[axis,0]**2 + J[axis,1]**2)
+    return norme
+
+#%%
+N = 7
+L = 2
+x10,x20 = -1,-1
+axis = 2
+
+grid = init_grid(N)
+X,Y = cen(grid)
+J = [jacobien(cell) for cell in grid]
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,0],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,1],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,2],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = J,cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+#%%
+for _ in range(2):
+    alpha = auto_threshold(grid)
+    print('alpha :',alpha)
+    grid = iterate_grid(grid,alpha)
+    X,Y = cen(grid)
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,0],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,1],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.xlim(x10,x10+L)
+plt.ylim(x20,x20+L)
+plt.scatter(X,Y,c = T_direct(X,Y)[:,2],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+#%% apprentissage de T sur grille non uniforme
+mesh = np.stack((X,Y),-1)
+data = T_direct(X,Y)
+# fit
+mlp_reg = MLPRegressor(hidden_layer_sizes=(150,120,80),
+                       max_iter = 300,activation = 'relu',
+                       solver = 'adam')
+
+mlp_reg.fit(mesh,data)
+
+
+# prediction 
+Nt = 30
+x = np.linspace(-1,1,Nt)
+X1,X2 = np.meshgrid(x,x)
+Xt = np.stack((X2,X1),-1)
+Xt = Xt.reshape(Nt*Nt,2)
+y_exact = T_direct(X1,X2)
+y_exact = y_exact.reshape(Nt*Nt,3)
+y_pred = mlp_reg.predict(Xt)
+
+
+
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = y_exact[:,axis],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = y_pred[:,axis],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+err = np.abs(y_exact[:,axis] - y_pred[:,axis])
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = err,cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+print('Mean Squared Error:', metrics.mean_squared_error(y_exact, y_pred))  
+
+#%% apprentissage de T sur grille uniforme
+N_uni = int(np.sqrt(len(grid)))
+x_uni = np.linspace(x10,x10+L,N_uni)
+X_uni,Y_uni = np.meshgrid(x_uni,x_uni)
+X_uni = X_uni.reshape(N_uni*N_uni)
+Y_uni = Y_uni.reshape(N_uni*N_uni)
+grid_uni = np.stack((X_uni,Y_uni),-1)
+data_uni = T_direct(X_uni,Y_uni)
+
+# fit
+mlp_reg_uni = MLPRegressor(hidden_layer_sizes=(100,150,60,10),
+                       max_iter = 300,activation = 'relu',
+                       solver = 'adam')
+
+mlp_reg_uni.fit(grid_uni,data_uni)
+
+# prediction
+y_pred = mlp_reg_uni.predict(Xt)
+
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = y_exact[:,axis],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = y_pred[:,axis],cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+err = np.abs(y_exact[:,axis] - y_pred[:,axis])
+plt.figure()
+plt.scatter(X1.flatten(),X2.flatten(),c = err,cmap = 'coolwarm')
+plt.axis('square')
+plt.colorbar()
+
+print('Mean Squared Error:', metrics.mean_squared_error(y_exact, y_pred))  
